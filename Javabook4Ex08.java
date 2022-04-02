53aa30c5e1e8c7dd1a3c7c3e9107a5edd5f34ad9
Inwoo Park <82872105+strikezwei@users.noreply.github.com>
2022-04-02 21:36:10 +0900

    Add files via upload

---
+++ Javabook4Ex08.java
@@ -0,0 +1,15 @@
+package ch04;
+
+
+public class Javabook4Ex08 {
+	public static void main(String[] args) {
+		// math.random()을 이요해서 1부터 6사이의 임의의 정수를 변수 value에 저장하는 코드를 완성
+		
+				int value = (int)(Math.random()*6)+1;
+				
+				System.out.println("value : "+ value);
+			}
+		}
+
+
+
