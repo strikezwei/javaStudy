1474e829e741297bfd43dc8c42b7ebb11510d5b2
Inwoo Park <82872105+strikezwei@users.noreply.github.com>
2022-04-05 21:09:23 +0900

    Add files via upload

---
+++ Javabook5Ex01.java
@@ -0,0 +1,14 @@
+package ch05;
+
+
+public class Javabook5Ex01 {
+	public static void main(String[] args) {
+		// 5-1 배열을 선언하거나 초기화한것 틀린것은? 5번
+		int[] arr[];
+		int[] arr1 = {1,2,3,4,5};
+		int[] arr2 = new int[5];
+//		int arr3[5];
+		int[] arr4[] = new int[3][];
+	}
+}
+
