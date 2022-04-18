242e868f3bc65bf67a3877ca23a5d90b380e7983
Inwoo Park <82872105+strikezwei@users.noreply.github.com>
2022-04-18 19:50:06 +0900

    Add files via upload

---
+++ Javabook6Ex01.java
@@ -0,0 +1,25 @@
+package ch06;
+
+
+public class Javabook6Ex01 {
+	public static void main(String[] args) {
+		// 6-1 다음과 같은 멤버변수를 갖는 student 클래스를 정의
+//		 String name 학생이름
+//		 int ban	반
+//		 int no		번호
+//		 int kor	국어점수
+//		 int eng	영어점수
+//		 int math	수학점수
+		
+		class student {
+			String name; 
+			 int ban;	
+			 int no	;	
+			 int kor;	
+			 int eng;	
+			 int math;	
+		}
+		
+	}
+}
+
